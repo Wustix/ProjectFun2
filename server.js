005dce92e78de604002cc76e1ff86ec780b0803b
Annette Yeager <annetteyeager@Annettes-MacBook-Pro.local>
2018-08-24 21:43:35 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var medthodOverride = require("method-overried");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+// Method override for RESTFUL form submissions
+app.use(medthodOverride("_method"));
+
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -21,6 +26,15 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+// Handlebars config
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("viwe engine", "handlebars");
+
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
@@ -29,8 +43,12 @@ require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.info(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
