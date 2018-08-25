20f2ec4ee2407279e26309941ed20291074558a0
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-25 10:26:38 -0500

    server changes

--- models/example.js
+++ models/example.js
@@ -1,7 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Example = sequelize.define("hmj82t0cjwpu9lri", {
+    category_list: DataTypes.STRING,
+    name_list: DataTypes.STRING,
+    price_list: DataTypes.STRING,
+    description_list: DataTypes.STRING,
+    email_list: DataTypes.STRING,
   });
   return Example;
 };
+// category_list VARCHAR (100),
+// 	name_list varchar(75) NOT NULL,
+// 	price_list INTEGER (6),
+// 	description_list VARCHAR (255),
+//     email_list VARCHAR (100),
