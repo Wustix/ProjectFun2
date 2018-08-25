9fb12aacbeaff694bcae53d6bbcce2b2a957c786
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-25 11:34:27 -0500

    re-work server

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-// Dependencies
-
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-// var medthodOverride = require("method-overried");
 
 // Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-// Method override for RESTFUL form submissions
-// app.use(medthodOverride("_method"));
-
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -26,28 +25,15 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
-// Handlebars config
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("viwe engine", "handlebars");
-
 // Routes
 // =============================================================
-require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.info(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
 });
