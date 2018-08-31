c0981dea041648c06459892adc663915293ac20c
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-31 17:27:56 -0500

    nodemailer

--- public/js/form.js
+++ public/js/form.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
   // Getting jQuery references to the post body, title, form, and category select
   var bodyInput = $("#body");
   var titleInput = $("#title");
+  var emailInput = $("#email");
   var priceInput = $("#price");
   var photoInput = $("#photo");
   var cmsForm = $("#cms");
@@ -25,12 +26,13 @@ $(document).ready(function() {
   $(cmsForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
     // Wont submit the post if we are missing a body or a title
-    if (!titleInput.val().trim() || !priceInput.val().trim() || !photoInput.val().trim() || !bodyInput.val().trim()) {
+    if (!titleInput.val().trim() || !emailInput.val().trim() || !priceInput.val().trim() || !photoInput.val().trim() || !bodyInput.val().trim()) {
       return;
     }
     // Constructing a newPost object to hand to the database
     var newPost = {
       title: titleInput.val().trim(),
+      email: emailInput.val().trim(),
       price: priceInput.val().trim(),
       photo: photoInput.val().trim(),
       body: bodyInput.val().trim(),
@@ -63,6 +65,7 @@ $(document).ready(function() {
       if (data) {
         // If this post exists, prefill our cms forms with its data
         titleInput.val(data.title);
+        emailInput.val(data.email);
         priceInput.val(data.price)
         bodyInput.val(data.body);
         postCategorySelect.val(data.category);
