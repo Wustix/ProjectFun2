43cd2e9e5767802481b57e4bf1632f8f1b7a1ef0
David Hoffmann <kc9gpj12@gmail.com>
2018-09-01 14:34:05 -0500

    nodemailer

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-var bodyParser = require("body-parser");
 var passport = require("passport");
 var passportSetup = require("./config/passport-setup.js");
 // Sets up the Express App
@@ -21,12 +20,6 @@ var PORT = process.env.PORT || 8000;
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static("public"));
