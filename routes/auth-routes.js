20fae641b1d82a5dc9c49ff49116a0af1a2f1cef
unknown <wroehrman@yahoo.com>
2018-09-03 21:22:45 -0500

    updates again

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -15,17 +15,8 @@ router.get("/google", passport.authenticate("google", {
     scope: ["profile"]
 }))
 
-<<<<<<< HEAD
+// router.get("/google"), passport.authenticate("google", { scope: "https://www.google.com/m8/feeds" });
 
-router.get("/google"),
-    passport.authenticate("google", { scope: "https://www.google.com/m8/feeds" });
-
-
-=======
- router.get("/google"),
-passport.authenticate("google" , { scope: "https://www.google.com/m8/feeds"});
- 
->>>>>>> 44d38b27c37a77aa069ad19f4d5bf7b0fa299a55
 //   handle with passport
 
 // callback route for google to redirect to
