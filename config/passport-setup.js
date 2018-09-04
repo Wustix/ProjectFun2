cd6f0df1a5c706e90f1860260dba7d7ed0741c7f
Annette Yeager <annetteyeager@Annettes-MacBook-Pro.local>
2018-09-03 19:11:06 -0500

    stuff

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -21,24 +21,6 @@ passport.use(
         console.log("tokens", accessToken, refreshToken);
         //  check if user already exists in our db
         console.log("authnticate with google");
-<<<<<<< HEAD
-        User.findOne({ googleId: profile.id }).then((currentUser) => {
-            if (currentUser) {
-                // already have the user
-                console.log("user is: ", currentUser);
-                done(null, currentUser);
-            } else {
-                // if not, then create user in our db
-                new User({
-                    username: profile.displayName,
-                    googleId: profile.id
-                }).save().then((newUser) => {
-                    console.log("new user created: " + newUser);
-                    done(null.newUser);
-                });
-            }
-        }); 
-=======
         // User.findOne({ googleId: profile.id }).then((currentUser) => {
         //     if (currentUser) {
         //         // already have the user
@@ -55,6 +37,5 @@ passport.use(
         //         });
         //     }
         // });
->>>>>>> 9e168358f313236cb1d540d7557ffd849c743bdb
     })
 )
